cda1513cc337b22766891fa2fa3e78067e52aa44
MartinAya <39680899+MartinAya@users.noreply.github.com>
2022-06-03 00:55:57 -0300

    Update votacion.js

--- votacion.js
+++ votacion.js
@@ -6,13 +6,25 @@ fun()
 
 async function fun() {
 
+    let ganador
+
     let nominaciones = await fs.readFileSync('nominaciones.txt', {encoding: 'utf8', flag: 'r'})
     nominaciones = nominaciones.split('\r\n')
     nominaciones = nominaciones.map(s => s.toLowerCase())
+    nominaciones = nominaciones.map((s => {
+        s = s.split('\'').join('')
+        s = s.split('’').join('')
+        return s
+    }))
 
     let votos = await fs.readFileSync('votos.txt', {encoding: 'utf8', flag: 'r'})
     votos = votos.split('\r\n')
     votos = votos.filter(s => s !== '')
+    votos = votos.map((s => {
+        s = s.split('\'').join('')
+        s = s.split('’').join('')
+        return s
+    }))
 
     let telefonos = votos.filter((_, index) => index % 2 === 0)
     votos = votos.filter((_, index) => index % 2 !== 0)
@@ -39,16 +51,44 @@ async function fun() {
         voto_.voto = peliculasVotadas.map(peliculaVotada => peliculaVotada.nominacion)
     })
 
-    votos = votos.map(voto => new Ballot(voto.telefono, voto.voto))
-
     console.log(`Total de votos: ${votos.length}`)
 
-    votos.forEach(ballot => console.log(ballot.owner, ballot.choices.toString()))
     console.log('=================================================')
 
-    let poll = new Poll(nominaciones, votos);
+    votos.forEach((voto, indice) => console.log(telefonos[indice], voto.voto.toString()))
 
-    let winner = poll.determineWinner();
+    console.log('=================================================')
 
-    console.log(winner);
+    // metodo ranked vote por puntaje
+
+    let puntajes = Object.fromEntries(nominaciones.map(pelicula => [pelicula, 0]))
+
+    votos.forEach(voto_ => {
+        voto_.voto.forEach((peliculaVotada, posicion) => {
+            let puntaje = 1 / (posicion + 1)
+            puntajes[peliculaVotada]+=puntaje
+        })
+    })
+
+    puntajes = Object.entries(puntajes)
+    puntajes = puntajes.sort((p1,p2) => p2[1] - p1[1])
+
+    console.log('Puntajes')
+
+    puntajes.forEach(puntaje => console.log(puntaje[0], ` -> ${puntaje[1]}`))
+
+    console.log('=================================================')
+
+    ganador = puntajes[0][0]
+
+    // metodo ranked vote por rondas
+
+    // votos = votos.map(voto => new Ballot(voto.telefono, voto.voto))
+
+    // let poll = new Poll(nominaciones, votos);
+    //
+    // ganador = poll.determineWinner();
+    //
+
+    console.log(`Ganador: ${ganador}`);
 }
